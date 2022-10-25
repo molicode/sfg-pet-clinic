050b89c2fac38c9db727ffc43481453fac5d5d6e
Luis Molina <molicode1347@gmail.com>
2022-10-25 16:45:30 -0300

    Refactor Service Interface to Common Base Interface. Closes #11

--- pet-clinic-data/src/main/java/molicode/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/molicode/springframework/sfgpetclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package molicode.springframework.sfgpetclinic.services;
 
-import java.util.Set;
-
 import molicode.springframework.sfgpetclinic.model.Vet;
 
-public interface VetService {
-
-  Vet findById(Long id);
-
-  Vet save(Vet vet);
-
-  Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
