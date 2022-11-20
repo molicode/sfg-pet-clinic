3ab17b582de18b2dd5f1c942dacce9e4e5f53cc3
Luis Molina <molicode1347@gmail.com>
2022-11-20 13:08:44 -0300

    added PetTupeService. closes #29

---
+++ pet-clinic-data/src/main/java/molicode/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package molicode.springframework.sfgpetclinic.services;
+
+import molicode.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
