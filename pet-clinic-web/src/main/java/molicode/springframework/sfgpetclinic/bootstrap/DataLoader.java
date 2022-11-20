8a3ab06db081aaa5d0ff204de7c2181f41e4eed7
Luis Molina <molicode1347@gmail.com>
2022-11-20 13:17:44 -0300

    Added PetTypes to bootstrap. closes #32

--- pet-clinic-web/src/main/java/molicode/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/molicode/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package molicode.springframework.sfgpetclinic.bootstrap;
 
 import molicode.springframework.sfgpetclinic.model.Owner;
+import molicode.springframework.sfgpetclinic.model.PetType;
 import molicode.springframework.sfgpetclinic.model.Vet;
 import molicode.springframework.sfgpetclinic.services.OwnerService;
+import molicode.springframework.sfgpetclinic.services.PetTypeService;
 import molicode.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,13 +16,25 @@ public class DataLoader implements CommandLineRunner {
 
   private final VetService vetService;
 
-  public DataLoader(OwnerService ownerService, VetService vetService) {
+  private final PetTypeService petTypeService;
+
+  public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
     this.ownerService = ownerService;
     this.vetService = vetService;
+    this.petTypeService = petTypeService;
   }
 
   @Override
   public void run(String... args) throws Exception {
+
+    PetType dog = new PetType();
+    dog.setName("Dog");
+    PetType savedDogPetType = petTypeService.save(dog);
+
+    PetType cat = new PetType();
+    cat.setName("Cat");
+    PetType savedCatPetType = petTypeService.save(cat);
+
     Owner owner1 = new Owner();
     owner1.setFirstName("Michael");
     owner1.setLastName("Weston");
