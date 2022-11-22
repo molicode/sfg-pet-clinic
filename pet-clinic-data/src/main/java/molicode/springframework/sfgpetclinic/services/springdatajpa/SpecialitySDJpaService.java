ab1c45f1a7df3b116f1d429c6c7187c21ecf957a
Luis Molina <molicode1347@gmail.com>
2022-11-22 10:54:18 -0300

    added spring data jpa service for Specialities. closes #44

---
+++ pet-clinic-data/src/main/java/molicode/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,48 @@
+package molicode.springframework.sfgpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import molicode.springframework.sfgpetclinic.model.Speciality;
+import molicode.springframework.sfgpetclinic.repositories.SpecialityRepository;
+import molicode.springframework.sfgpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialtyService {
+
+  private final SpecialityRepository specialityRepository;
+
+  public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+    this.specialityRepository = specialityRepository;
+  }
+
+  @Override
+  public Set<Speciality> findAll() {
+    Set<Speciality> specialities = new HashSet<>();
+    specialityRepository.findAll().forEach(specialities::add);
+    return specialities;
+  }
+
+  @Override
+  public Speciality findById(Long id) {
+    return specialityRepository.findById(id).orElse(null);
+  }
+
+  @Override
+  public Speciality save(Speciality object) {
+    return specialityRepository.save(object);
+  }
+
+  @Override
+  public void delete(Speciality object) {
+    specialityRepository.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    specialityRepository.deleteById(id);
+  }
+}
