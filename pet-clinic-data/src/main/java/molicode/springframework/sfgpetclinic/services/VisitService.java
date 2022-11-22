8162b43cd33264693431c71aa15685d82ec2a2d0
Luis Molina <molicode1347@gmail.com>
2022-11-22 12:09:05 -0300

    implemented map impl for visit service. closes #46

---
+++ pet-clinic-data/src/main/java/molicode/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package molicode.springframework.sfgpetclinic.services;
+
+import molicode.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
