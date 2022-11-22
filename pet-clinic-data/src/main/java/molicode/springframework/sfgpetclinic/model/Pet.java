c75fad549818365d4eb20d66da705fba2a6985a2
Luis Molina <molicode1347@gmail.com>
2022-11-22 17:54:27 -0300

    Added Project Lombok to project. closes #51

--- pet-clinic-data/src/main/java/molicode/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/molicode/springframework/sfgpetclinic/model/Pet.java
@@ -11,10 +11,32 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
+
+  @Builder
+  public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+    super(id);
+    this.name = name;
+    this.petType = petType;
+    this.owner = owner;
+    this.birthDate = birthDate;
+
+//    if (visits == null || visits.size() > 0) {
+//      this.visits = visits;
+//    }
+  }
 
   @Column(name = "name")
   private String name;
@@ -33,35 +55,4 @@ public class Pet extends BaseEntity {
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
   private Set<Visit> visits = new HashSet<>();
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public PetType getPetType() {
-    return petType;
-  }
-
-  public void setPetType(PetType petType) {
-    this.petType = petType;
-  }
-
-  public Owner getOwner() {
-    return owner;
-  }
-
-  public void setOwner(Owner owner) {
-    this.owner = owner;
-  }
-
-  public LocalDate getBirthDate() {
-    return birthDate;
-  }
-
-  public void setBirthDate(LocalDate bithDate) {
-    this.birthDate = bithDate;
-  }
 }
--- pet-clinic-data/src/main/java/molicode/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/molicode/springframework/sfgpetclinic/model/Speciality.java
@@ -3,7 +3,17 @@ package molicode.springframework.sfgpetclinic.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialities")
 public class Speciality extends BaseEntity {
@@ -11,11 +21,5 @@ public class Speciality extends BaseEntity {
   @Column(name = "description")
   private String description;
 
-  public String getDescription() {
-    return description;
-  }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
 }
